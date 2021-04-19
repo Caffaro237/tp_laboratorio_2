79415ae2f64cec4ec22d7247ec5f8b18a6e09ee3
Caffaro237 <facundodarian3@gmail.com>
2021-04-19 18:58:41 -0300

    Trabajo Practico Numero 1

---
+++ Trabajo Practico Numero 1/Calculadora/FormCalculadora.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace MiCalculadora
+{
+    partial class FrmMiCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMiCalculadora));
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnDecimalBinario = new System.Windows.Forms.Button();
+            this.btnBinarioDecimal = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(92, 105);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(193, 51);
+            this.btnOperar.TabIndex = 4;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(291, 105);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(193, 51);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(490, 105);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(193, 51);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnDecimalBinario
+            // 
+            this.btnDecimalBinario.Location = new System.Drawing.Point(92, 179);
+            this.btnDecimalBinario.Name = "btnDecimalBinario";
+            this.btnDecimalBinario.Size = new System.Drawing.Size(291, 51);
+            this.btnDecimalBinario.TabIndex = 7;
+            this.btnDecimalBinario.Text = "Convertir a Binario";
+            this.btnDecimalBinario.UseVisualStyleBackColor = true;
+            this.btnDecimalBinario.Click += new System.EventHandler(this.btnDecimalBinario_Click);
+            // 
+            // btnBinarioDecimal
+            // 
+            this.btnBinarioDecimal.Location = new System.Drawing.Point(389, 180);
+            this.btnBinarioDecimal.Name = "btnBinarioDecimal";
+            this.btnBinarioDecimal.Size = new System.Drawing.Size(294, 50);
+            this.btnBinarioDecimal.TabIndex = 8;
+            this.btnBinarioDecimal.Text = "Convertir a Decimal";
+            this.btnBinarioDecimal.UseVisualStyleBackColor = true;
+            this.btnBinarioDecimal.Click += new System.EventHandler(this.btnBinarioDecimal_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(92, 29);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(220, 49);
+            this.txtNumero1.TabIndex = 1;
+            this.txtNumero1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero1_KeyPress);
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(463, 30);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(220, 49);
+            this.txtNumero2.TabIndex = 3;
+            this.txtNumero2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero2_KeyPress);
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbOperador.Location = new System.Drawing.Point(336, 29);
+            this.cmbOperador.Name = "cmbOperador";
+            this.cmbOperador.Size = new System.Drawing.Size(111, 50);
+            this.cmbOperador.TabIndex = 2;
+            this.cmbOperador.Text = "+";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(84, 246);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(191, 44);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // FrmMiCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 334);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cmbOperador);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnBinarioDecimal);
+            this.Controls.Add(this.btnDecimalBinario);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMiCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Facundo Caffaro 2°C";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMiCalculadora_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnDecimalBinario;
+        private System.Windows.Forms.Button btnBinarioDecimal;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.ComboBox cmbOperador;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Trabajo Practico Numero 1/Calculadora/FormCalculadora.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FrmMiCalculadora : Form
+    {
+        #region Metodos
+
+        public FrmMiCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            cmbOperador.Text = "";
+            lblResultado.Text = "Resultado";
+        }
+
+        private static double Operar(string strNumero1, string strNumero2, string operador)
+        {
+            Numero numero1 = new Numero(strNumero1);
+            Numero numero2 = new Numero(strNumero2);
+
+            return Calculadora.Operar(numero1, numero2, operador);
+        }
+
+        #endregion
+
+        #region Eventos
+
+        /// <summary>
+        /// Evento de cierre del Formulario preguntando si se quiere cerrar o no
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmMiCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro?", 
+                "Cerrando Calculadora", 
+                MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+
+        #region Botones
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double respuesta = 0;
+
+            respuesta = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+
+            this.lblResultado.Text = respuesta.ToString();
+        }
+
+
+        private void btnDecimalBinario_Click(object sender, EventArgs e)
+        {
+            Numero numero1 = new Numero();
+
+            this.lblResultado.Text = numero1.DecimalBinario(this.lblResultado.Text);
+        }
+        
+
+        private void btnBinarioDecimal_Click(object sender, EventArgs e)
+        {
+            Numero numero1 = new Numero();
+
+            this.lblResultado.Text = numero1.BinarioDecimal(this.lblResultado.Text);
+        }
+
+        #endregion
+
+        #region TextBox error letras
+
+        /// <summary>
+        /// Verifica que no se ingresen letras para que el programa no de error
+        /// Tambien se verifico que solo se pueda ingresar un solo . para los numeros con decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNumero1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar.ToString().Equals("."))
+            {
+                foreach (char item in this.txtNumero1.Text)
+                {
+                    if(item == '.')
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Solo se puede ingresar un solo punto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+            }
+            else if (e.KeyChar.ToString().Equals("-"))
+            {
+                foreach (char item in this.txtNumero1.Text)
+                {
+                    if (item == '-')
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Solo se puede ingresar un solo menos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Ingresar solo numeros o numeros decimales", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Verifica que no se ingresen letras para que el programa no de error
+        /// Tambien se verifico que solo se pueda ingresar un solo . para los numeros con decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtNumero2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar.ToString().Equals("."))
+            {
+                foreach (char item in this.txtNumero2.Text)
+                {
+                    if (item == '.')
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Solo se puede ingresar un solo punto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+            }
+            else if (e.KeyChar.ToString().Equals("-"))
+            {
+                foreach (char item in this.txtNumero2.Text)
+                {
+                    if (item == '-')
+                    {
+                        e.Handled = true;
+                        MessageBox.Show("Solo se puede ingresar un solo menos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                    }
+                }
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Ingresar solo numeros o numeros decimales", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+    }
+}
