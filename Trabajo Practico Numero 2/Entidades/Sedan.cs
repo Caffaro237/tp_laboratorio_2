63ad89a3e03d61f9b61488ec3519c8727da7bb52
Caffaro237 <facundodarian3@gmail.com>
2021-05-08 19:32:02 -0300

    Trabajo Practico Numero 2

---
+++ Trabajo Practico Numero 2/Entidades/Sedan.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        #region Atributos
+
+        ETipo tipo;
+
+        #endregion
+
+        #region Enumerados
+
+        public enum ETipo 
+        {
+            CuatroPuertas, 
+            CincoPuertas 
+        }
+
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// Constructor que inicializa la variable tipo
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+           :this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Retorna los datos del vehiculo tipo SEDAN
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendFormat("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Trabajo Practico Numero 2/Entidades/Suv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        #region Constructores
+
+        /// <summary>
+        /// Unico constructor que inicializa los datos heredados
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis, marca, color)
+        {
+
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Retorna los datos del vehiculo tipo SUV
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
